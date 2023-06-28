e30dbf976a36fed7371383a97c8993f40c690d07
Tyler Stewart <tyler.stewart@voiceflow.com>
2023-06-28 10:10:22 -0600

    feat: add forRoot to module (#1)
    * feat: add forRoot to module
    
    * chore: github test workflow

--- src/event.module-definition.ts
+++ src/event.module-definition.ts
@@ -2,5 +2,9 @@ import { ConfigurableModuleBuilder } from "@nestjs/common";
 
 import { EventModuleOptions } from "./event.module-options";
 
-export const { ConfigurableModuleClass, MODULE_OPTIONS_TOKEN } =
-  new ConfigurableModuleBuilder<EventModuleOptions>().build();
+export const {
+  ConfigurableModuleClass,
+  MODULE_OPTIONS_TOKEN,
+  OPTIONS_TYPE,
+  ASYNC_OPTIONS_TYPE,
+} = new ConfigurableModuleBuilder<EventModuleOptions>().build();
